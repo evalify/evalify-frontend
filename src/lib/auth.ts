c9682e824bfc44ff265feef5e6e95fa6dd3598d6
TharunCodes07 <senthietk@gmail.com>
2025-05-15 15:50:14 +0530

    Fixed session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,60 +1,34 @@
-
-import NextAuth from "next-auth"
-import Keycloak from "next-auth/providers/keycloak"
-import jwt from "jsonwebtoken"
+import NextAuth from "next-auth";
+import Keycloak from "next-auth/providers/keycloak";
+import { decode } from "jsonwebtoken";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        Keycloak({
-            profile: (profile) => {
-                console.log(profile)
-                const roles = profile?.realm_access?.roles || [];
-                return {
-                    id: profile.id,
-                    name: profile.name,
-                    email: profile.email,
-                    roles,
-                };
-            }
-        })
-    ],
-    pages: {
-        signIn: "/auth/login",
+  providers: [
+    Keycloak({
+      clientId: process.env.AUTH_KEYCLOAK_ID,
+      clientSecret: process.env.AUTH_KEYCLOAK_SECRET,
+      issuer: process.env.AUTH_KEYCLOAK_ISSUER,
+    }),
+  ],
+  pages: {
+    signIn: "/auth/login",
+  },
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.access_token) {
+        const decoded = decode(account.access_token);
+        if (decoded && typeof decoded === "object") {
+          token.roles = decoded.realm_access?.roles || [];
+        }
+      }
+      return token;
     },
-    callbacks: {
-        async jwt({ token, user, account, profile }) {
-
-            console.log("account " + JSON.stringify(account))
-            let roles = undefined as
-                | { [key: string]: { roles: Array<any> } }
-                | undefined
-
-
-            if (account?.access_token) {
-                let decodedToken = jwt.decode(account?.access_token)
-                if (decodedToken && typeof decodedToken !== 'string') {
-                    roles = decodedToken?.realm_access
-                }
-            }
-            token = { ...token, roles: roles?.roles }
-            console.log("[jwt callback] token " + JSON.stringify(token))
-            return token;
-        },
-        session: async ({ session, token, user }) => {
-            if (session?.user) {
-                session.user.id = token.sub;
-            }
-            console.log("[session callback] token " + JSON.stringify(token))
-
-            return session;
-        },
-    }
-
-})
-
-
-declare module "next-auth" {
-    interface User {
-        role?: string;
-    }
-}
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub as string;
+        session.user.roles = token.roles as string[];
+      }
+      return session;
+    },
+  },
+});
