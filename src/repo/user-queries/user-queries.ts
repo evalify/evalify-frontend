28a911785fb89352e0377033f286bc497be90420
TharunCodes07 <senthietk@gmail.com>
2025-07-10 16:27:25 +0530

    feat: Bank Share

--- src/repo/user-queries/user-queries.ts
+++ src/repo/user-queries/user-queries.ts
@@ -82,6 +82,11 @@ const userQueries = {
       ? response.data
       : response.data.data || [];
   },
+
+  fetchAllStaff: async (): Promise<User[]> => {
+    const response = await axiosInstance.get("/api/user/faculty/all");
+    return response.data;
+  },
 };
 
 export default userQueries;
