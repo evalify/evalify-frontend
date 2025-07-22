2e1f388aab4c9f4b2a3a90645de799718f8d257d
Aksay Kanthan <aksaykanthan@gmail.com>
2025-07-22 14:10:29 +0530

    enhance: redeisgned sidebar

--- src/app/(evalify)/layout.tsx
+++ src/app/(evalify)/layout.tsx
@@ -1,9 +1,5 @@
 import type { Metadata } from "next";
-import {
-  SidebarProvider,
-  SidebarInset,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/navigation/side-navbar/side-navbar";
 import AuthGuard from "@/components/auth/auth-guard";
 import { UserType } from "@/lib/auth/utils";
@@ -31,11 +27,11 @@ export default function RootLayout({
       <SidebarProvider>
         <AppSidebar />
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          {/* <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2 px-4 flex-row">
               <SidebarTrigger className="-ml-1" />
             </div>
-          </header>
+          </header> */}
           <div className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</div>
         </SidebarInset>
       </SidebarProvider>
